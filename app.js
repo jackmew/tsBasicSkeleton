44b8f0bd36c237fadd0ca17e368bee0f8e5bca54
ZestHo <zest6789tower@gmail.com>
2018-08-22 23:49:14 +0800

    Basic Types

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("TypeScript start");
+// string
+var myName = "zest";
+// number
+var myAge = 30;
+// boolean
+var isCrazy = false;
+// array
+var bosses = ["Jason", "Michael"];
+// tuples
+var myExam = ["math", 98];
+console.log(myExam[0]);
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 100] = "Gray";
+    Color[Color["Green"] = 101] = "Green";
+    Color[Color["Blue"] = 102] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Gray;
+console.log(myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car.brand);
--- app.ts
+++ app.ts
@@ -1,1 +1,30 @@
-console.log("TypeScript start");
+// string
+let myName: string = "zest";
+
+// number
+let myAge: number = 30;
+
+// boolean
+let isCrazy: boolean = false;
+
+// array
+let bosses: string[] = ["Jason", "Michael"];
+
+// tuples
+let myExam: [string, number] = ["math", 98];
+console.log(myExam[0]);
+
+// enum
+enum Color {
+  Gray = 100,
+  Green,
+  Blue
+}
+let myColor: Color = Color.Gray;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car.brand);
