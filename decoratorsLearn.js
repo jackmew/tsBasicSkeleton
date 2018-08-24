903884215cb9dc040403c72170bed93f91ba28f4
ZestHo <zest6789tower@gmail.com>
2018-08-24 15:12:19 +0800

    different type of decorator

--- decoratorsLearn.js
+++ decoratorsLearn.js
@@ -1,48 +1,96 @@
+// function logged(constructorFn: Function) {
+//     console.log(constructorFn);
+// }
+// @logged
+// class Animal {
+//     constructor() {
+//         console.log("Hi !");
+//     }
+// }
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function logged(constructorFn) {
-    console.log(constructorFn);
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+// // Factory
+// function logging(value: boolean) {
+//     return value ? logged : null
+// }
+// @logging(true)
+// class Car {
+// }
+// // Advanced
+// function printable(constructorFn: Function) {
+//     constructorFn.prototype.print = function() {
+//         console.log(this);
+//     }
+// }
+// @logging(true)
+// @printable
+// class Plane {
+//     name = "Green Plane"
+// }
+// const plane = new Plane();
+// (<any>plane).print();
+// // Method Decorator
+// function editable(value: boolean) {
+//     return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+//         descriptor.writable = value;
+//     }
+// }
+// function overwritable(value: boolean) {
+//     return function(target: any, propName: string): any {
+//         const newDescriptor: PropertyDescriptor = {
+//             writable: value
+//         };
+//         return newDescriptor;
+//     }
+// }
+// class ZestProject {
+//     @overwritable(false)
+//     projectName: string;
+//     constructor(name: string) {
+//         this.projectName = name;
+//     }
+//     @editable(false)
+//     calcBudge() {
+//         console.log(1000);
+//     }
+// }
+// const zProject = new ZestProject('Zest Project');
+// zProject.calcBudge();
+// zProject.calcBudge = function() {
+//     console.log(2000);
+// }
+// zProject.calcBudge();
+// console.log(zProject);
+// Parameter Decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log("Target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("paramIndex: ", paramIndex);
 }
-var Animal = /** @class */ (function () {
-    function Animal() {
-        console.log("Hi !");
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name = name;
     }
-    Animal = __decorate([
-        logged
-    ], Animal);
-    return Animal;
-}());
-// Factory
-function logging(value) {
-    return value ? logged : null;
-}
-var Car = /** @class */ (function () {
-    function Car() {
-    }
-    Car = __decorate([
-        logging(true)
-    ], Car);
-    return Car;
-}());
-// Advanced
-function printable(constructorFn) {
-    constructorFn.prototype.print = function () {
-        console.log(this);
+    Course.prototype.printStudentNumbers = function (mode, printAll) {
+        if (printAll) {
+            console.log(10000);
+        }
+        else {
+            console.log(2000);
+        }
     };
-}
-var Plane = /** @class */ (function () {
-    function Plane() {
-        this.name = "Green Plane";
-    }
-    Plane = __decorate([
-        logging(true),
-        printable
-    ], Plane);
-    return Plane;
+    __decorate([
+        __param(1, printInfo)
+    ], Course.prototype, "printStudentNumbers", null);
+    return Course;
 }());
-var plane = new Plane();
-plane.print();
+var course = new Course("Super course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
--- decoratorsLearn.ts
+++ decoratorsLearn.ts
@@ -1,34 +1,94 @@
-function logged(constructorFn: Function) {
-    console.log(constructorFn);
+// function logged(constructorFn: Function) {
+//     console.log(constructorFn);
+// }
+// @logged
+// class Animal {
+//     constructor() {
+//         console.log("Hi !");
+//     }
+// }
+
+// // Factory
+// function logging(value: boolean) {
+//     return value ? logged : null
+// }
+
+// @logging(true)
+// class Car {
+
+// }
+
+// // Advanced
+// function printable(constructorFn: Function) {
+//     constructorFn.prototype.print = function() {
+//         console.log(this);
+//     }
+// }
+// @logging(true)
+// @printable
+// class Plane {
+//     name = "Green Plane"
+// }
+// const plane = new Plane();
+// (<any>plane).print();
+
+// // Method Decorator
+// function editable(value: boolean) {
+//     return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+//         descriptor.writable = value;
+//     }
+// }
+// function overwritable(value: boolean) {
+//     return function(target: any, propName: string): any {
+//         const newDescriptor: PropertyDescriptor = {
+//             writable: value
+//         };
+//         return newDescriptor;
+//     }
+// }
+
+// class ZestProject {
+//     @overwritable(false)
+//     projectName: string;
+
+//     constructor(name: string) {
+//         this.projectName = name;
+//     }
+//     @editable(false)
+//     calcBudge() {
+//         console.log(1000);
+//     }
+// }
+// const zProject = new ZestProject('Zest Project');
+// zProject.calcBudge();
+// zProject.calcBudge = function() {
+//     console.log(2000);
+// }
+// zProject.calcBudge();
+// console.log(zProject);
+
+// Parameter Decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+    console.log("Target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("paramIndex: ", paramIndex);
 }
-@logged
-class Animal {
-    constructor() {
-        console.log("Hi !");
+
+class Course {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    printStudentNumbers(mode: string, @printInfo printAll: boolean) {
+        if (printAll) {
+            console.log(10000);
+        } else {
+            console.log(2000);
+        }
     }
 }
-
-// Factory
-function logging(value: boolean) {
-    return value ? logged : null
-}
-
-@logging(true)
-class Car {
-
-}
-
-// Advanced
-function printable(constructorFn: Function) {
-    constructorFn.prototype.print = function() {
-        console.log(this);
-    }
-}
-@logging(true)
-@printable
-class Plane {
-    name = "Green Plane"
-}
-const plane = new Plane();
-(<any>plane).print();
-
+const course = new Course("Super course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
